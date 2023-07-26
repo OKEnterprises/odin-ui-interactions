2f2e892b7949a3a002ccab5852075ac92358b97d
Garret O'Connor <okenerenterprises@gmail.com>
2023-07-25 23:53:28 -0400

    Changed file structure; began work on image-carousel

---
+++ image-carousel/index.js
@@ -0,0 +1,7 @@
+const navOne = document.querySelector("#nav-1")
+const navTwo = document.querySelector("#nav-2")
+const navThree = document.querySelector("#nav-3")
+
+navOne.addEventListener('click', () => {
+    
+});
